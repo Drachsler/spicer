d260f4d92e2c99e31ec605f623b099c0fdabc548
Drachsler <34274437+Drachsler@users.noreply.github.com>
2025-04-14 23:35:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ document.addEventListener('DOMContentLoaded', async () => {
                 lastOnline: userData.lastOnline
               };
               sessionStorage.setItem("sessionUser", JSON.stringify(sessionUser));
-              // window.location.href = `details.html?id=${spiceId}&user=${enteredPin}`;
+              window.location.href = "details.html";
             } else {
               console.warn("❌ Kein Benutzer mit diesem PIN gefunden");
               showError("No user found",false);
