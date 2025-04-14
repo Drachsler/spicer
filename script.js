02863021ae4f84bd1fe5c8224bd03c3ee9182257
Drachsler <34274437+Drachsler@users.noreply.github.com>
2025-04-14 23:25:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("B");
+console.log("C");
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
 import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
